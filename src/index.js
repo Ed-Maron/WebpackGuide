4e0a3c4b4fb6a700dd6ab751ce0e0d7680a6addc
Igor Lygin <lazycoast102@gmail.com>
2019-02-08 01:01:41 +0400

    Output Management

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
 	let element = document.createElement('div');
+	let btn = document.createElement('button');
+
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 	element.classList.add('hello');
 
-	var myIcon = new Image();
-	myIcon.src = Icon;
-
-	element.appendChild(myIcon);
+	// let myIcon = new Image();
+	// myIcon.src = Icon;
+	
+	btn.innerHTML = 'Click me and check the console!';
+	btn.onclick = printMe;
+	element.appendChild(btn);
 
 	return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
-	entry: './src/index.js',
+	entry: {
+		app: './src/index.js',
+		print: './src/print.js'
+	},
+	plugins: [
+		new CleanWebpackPlugin(['dist']),
+		new HtmlWebpackPlugin({
+			title: 'Output Management'
+		})
+	],
 	output: {
-		filename: 'bundle.js',
+		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist')
 	},
 	module: {
@@ -16,11 +26,13 @@ module.exports = {
 				]
 			},
 			{
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      }
+				test: /\.(png|svg|jpg|gif)$/,
+				use: ['file-loader']
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: ['file-loader']
+			}
 		]
 	}
 };
